f88ca46f78684f82fea4a0a86d282b4628297af0
Lucas Gomes <lucasribeiroo1703@gmail.com>
2023-10-13 15:25:48 -0300

    Version 2.0
    Alteração de modo geral na pagina varias alterações acumuladas

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Portifolio
  Meu portifolio pessoal,
  desenvolvido com Html, Css e JavaScript. Link do github pages: lucasgr2.github.io/Portifolio
+ 
--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
-// Menu dinamico
-const menuIcon = document.querySelector('.cabecalho__icon');
-const menu = document.querySelector('.cabecalho__menu');
-const body = document.body;
-
-// Função para fechar o menu
-function closeMenu() {
-  menuIcon.classList.remove('active');
-  menu.classList.remove('active');
-  body.style.overflow = ''; // Restaura o comportamento de scroll normal
-}
-
-menuIcon.addEventListener('click', function (event) {
-  event.stopPropagation(); // Impede que o evento de clique se propague para o documento
-  menuIcon.classList.toggle('active');
-  menu.classList.toggle('active');
-
-  if (menu.classList.contains('active')) {
-    body.style.overflow = 'hidden'; // Impede o scroll da página quando o menu está aberto
+// CÓDIGO PARA ATIVAR E REMOVER O BOTÃO PARA VOLTAR AO TOPO DO SITE
+var botao = document.querySelector("#retroceder");
+window.onscroll = function(){
+  var altura = window.scrollY;
+  console.log(altura)
+  if (altura >=1000) {
+    botao.classList.remove("retroceder--disable");
+    botao.classList.add("retroceder--active");
   } else {
-    body.style.overflow = ''; // Restaura o comportamento de scroll normal
+    botao.classList.remove("retroceder--active");
   }
-});
+};
 
-body.addEventListener('click', function (event) {
-  if (!menu.contains(event.target) && menu.classList.contains('active')) {
-    closeMenu();
-  }
-});
+// CÓDIGO PARA ADICIONAR EFEITO NA ROLAGEM DA PAGINA
 
+const elements = document.querySelectorAll(".hidden")
+
+const monitor = new IntersectionObserver((entries)=> {
+    entries.forEach((entrie)=>{
+      if (entrie.isIntersecting) {
+        entrie.target.classList.add("show")
+      } else {
+        entrie.target.classList.remove("show")
+      }
+    })
+})
+
+elements.forEach((element)=>{monitor.observe(element)})
